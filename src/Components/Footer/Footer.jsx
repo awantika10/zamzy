1b6fd0534f2c115a787f076b8d3bf13403483665
weebo-ary <aryan.bhardwaj1233@gmail.com>
2021-12-17 23:39:26 +0530

    updateFooter

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Logo from "../../assets/images/Logo.png"
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='Footer'>
+            <div className='footer-profile'>
+                <img src = {Logo} className = "logoFooter img-fluid" alt='footerImg' />
+                <div className = "footerTxt">
+                <h4>Home</h4>
+                <h4>Stories</h4>
+                <h4>Consult</h4>
+            </div>
+            <div className="footerCopy">
+                <h4>Copyright by Team sOuls ©2021-2022 </h4>
+            </div>
+            </div>
+            <div className='footer-profile' style = {{margin: "10px"}}>
+            <p>Made with 💝 by Aryan & Awantika</p>
+            </div>
         </div>
     )
 }
